185f7c985d460a111f3e5ed24b9b4d36092bfd2b
SebastianNCalvo <sebaacalvoo@gmail.com>
2026-03-14 21:19:52 -0300

    Solucionado el post en el cart. El cart ya no duplica objetos, sino que los va agregando de a 1 a la cantidad

--- routes/cart.router.js
+++ routes/cart.router.js
@@ -17,17 +17,19 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const Id = req.body.id;
-        const {name, price, category} = await Products.findById(Id);
         if(!mongoose.Types.ObjectId.isValid(Id)){
             return res.status(400).json({error: 'El formato del id no es valido'})
         }
-        const segundoNombre= Cart.findOne(name)
-        // if (name === Cart.findOne(name)){
-
-        // }
-        // const newCart = new Cart ({name, price, category});
-        // await newCart.save()
-        res.status(200).json({segundoNombre: segundoNombre})
+        const {name, price, category} = await Products.findById(Id);
+        const isInCart = await Cart.findOne({name: name});
+        if (isInCart) {
+            isInCart.quantity +=1
+            res.status(200).json({message: 'Se sumó 1 más'})
+        } else {
+            const newCart = new Cart ({name, price, category});
+            await newCart.save()
+            res.status(200).json({newCart: newCart})
+        }
     } catch (err) {
         return res.status(500).json({error: 'Error interno del servidor', message: err.message})
     }
